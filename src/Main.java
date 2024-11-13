2d14c746185218c79ce3a9bd2224fd1a7b21bc00
Vivian.Potts <luc.potts@outlook.com>
2024-11-13 15:31:49 +0000

    3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         enemyBuilder enemyBuilder = new enemyBuilder();
+        ArrayList<Integer> playerInventory = new ArrayList<>();
+        int playerGold = 0;
+        playerInventory.add(1);
+        playerInventory.add(1);
+
 
 
 
--- src/enemyBuilder.java
+++ src/enemyBuilder.java
@@ -1,6 +1,6 @@
 public class enemyBuilder {
     public Enemy buildSlime(){
-        Enemy slime = new Enemy(12,"Slime", new int[][]{{1,2},{1,2}},null,null,null,2,"Gun");
+        Enemy slime = new Enemy(12,"Slime", new int[][]{{1,2},{1,2}},null,null,null,2,0);
         return  slime;
     }
     //test
